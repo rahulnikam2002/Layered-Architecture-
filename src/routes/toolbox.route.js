b5c7639c34446dc7328bcce8eea6a89d9cb8684d
Rahul Nikam <79020081+rahulnikam2002@users.noreply.github.com>
2025-11-05 02:54:08 +0530

    fix: correct typo in BaseRepository query execution method and implement ToolController with CRUD operations (#5)

--- src/routes/toolbox.route.js
+++ src/routes/toolbox.route.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const ToolController = require("../controllers/ToolController");
 const router = express.Router()
 
+const toolController = new ToolController()
 
-router.get("/", (req, res) => {
-    res.json({
-        success: false,
-        message: "Toolbox API is working!"
-    })
-})
+// localhost:5000/api/tools/
+router.get("/", toolController.getAllTools)
+
+// localhost:5000/api/tools/popular
+router.get("/popular", toolController.getPopularTools)
+
+// localhost:5000/api/tools/category/frontend
+router.get('/category/:category', toolController.getToolsByCategory);
+
+router.get('/:id', toolController.getToolById);
+
+router.post('/create', toolController.createTool);
+
+router.post('/create/bulk', toolController.createBulkTools);
+
+router.put('/:id', toolController.updateTool);
+
+router.delete('/delete/bulk', toolController.deleteBulkTools);
+
+router.delete('/delete/:id', toolController.deleteTool);
 
 module.exports = router
